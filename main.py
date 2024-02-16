7ded837f2d4e33274dde424fa41af3e728edc586
IndiraPe <forever_tk_16@hotmail.com>
2024-02-15 23:13:56 -0500

    ADD: init of game

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-msg = "Kiraaaa"
-print(msg)
+import random
+
+numRandom = random.randint(1, 100)
+
+namePlayer = input('Hola!!, ingresa tu nombre: ')
+print('Bienvenido(a) ' + namePlayer + ', intenta ganarle a la computadora adivinando el número primero. Comencemos!!!\nYa tengo el número, Tu empiezas!!!')
+numOfPlayer = input('Adivina el número: ')
+
+if int(numOfPlayer) == numRandom:
+    print('Felicidades!!! Venciste a la computadora')
+elif int(numOfPlayer) > numRandom:
+    print('Es muy alto!!')
+else:
+    print('Es muy bajo!!')
